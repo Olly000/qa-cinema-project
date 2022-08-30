e731d0854c7648e8f291c6ba5657abe5b6b6d42d
olly000 <ollystockman@gmail.com>
2022-08-30 16:19:12 +0100

    CP-12 #in-progress basic classification page layout

---
+++ qa-cinema-project/src/components/Classification/Classification.jsx
@@ -0,0 +1,15 @@
+import Links from './Links.jsx';
+import ClassText from "./ClassText.jsx";
+
+const Classification = () => {
+
+
+    return(
+        <>
+            <ClassText />
+            <Links />
+        </>
+    )
+}
+
+export default Classification;
--- qa-cinema-project/src/index.js
+++ qa-cinema-project/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Classification from "./components/Classification/Classification";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Classification />
   </React.StrictMode>
 );
 
