c4f921ac334e36e0b0df97461149bfc6b32e57ae
olly000 <ollystockman@gmail.com>
2022-08-31 19:53:59 +0100

    removed api key from .jsx file

--- src/components/GettingThere/Map.jsx
+++ src/components/GettingThere/Map.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import {useState} from 'react';
 import BingMapsReact from 'bingmaps-react';
 
 const Map = () => {
+
+    const [bingMapReady, setBingMapReady] = useState(false) // added a new state to use in bingmapsreact
 
     const pushPin = {
         center: {
@@ -15,12 +17,18 @@ const Map = () => {
 
     const pushPins = [pushPin];
 
+
     return (
         <BingMapsReact
-            bingMapsKey='Ah_VEsrpAML8op6IOZ0g3b8d-czZ7uDps3SAcAXYver7MCvmUj60LMX9kmxH-kd3'
+            pushPins={bingMapReady ? pushPins : null}
+            onMapReady={function () {
+                setBingMapReady(true)
+            }}
+            bingMapsKey={process.env.REACT_APP_MAP_API_KEY}
             height="20em"
             width="20em"
-            pushPins={pushPins}
+
+
             viewOptions={{
                 center: {latitude: 51.504860, longitude: -0.068170},
                 zoom: 14
